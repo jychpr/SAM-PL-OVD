e818c5481475cfd74683a1782624c0cf799cdbe0
jychpr <joy.chr.pr@gmail.com>
2026-04-16 12:32:26 +0800

    v6 blind concat detr box and sam priors

--- scripts/train_proposed_RN50_LONG.sh
+++ scripts/train_proposed_RN50_LONG.sh
@@ -2,13 +2,12 @@
 
 output_dir=$1
 
-# Override PyTorch 2.6 security check natively if needed
 export TORCH_ALLOW_CLASSES="argparse.Namespace"
 
 torchrun --nproc_per_node=1 --master_port=29500 main.py \
     --output_dir $output_dir \
     -c config/OV_COCO/OVDQUO_RN50.py \
-    --options dataset_file=ovcoco \
+    --options dataset_file=ovcoco text_len=77 \
     --amp \
     --eval_start_epoch 5 \
     --eval_every_epoch 5
