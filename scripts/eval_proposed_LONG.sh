fa2d5714cbe7a32971eef6aec905f93534b50455
jychpr <joy.chr.pr@gmail.com>
2026-04-19 18:28:38 +0800

    revise eval scripts

--- scripts/eval_proposed_LONG.sh
+++ scripts/eval_proposed_LONG.sh
@@ -1,11 +1,23 @@
+# #!/bin/bash
+
+# export TORCH_ALLOW_CLASSES="argparse.Namespace"
+
+# # Notice the --eval flag and the --resume pointing to your finished V6 weights
+# torchrun --nproc_per_node=1 --master_port=29500 main.py \
+#     --output_dir logs/proposed_r50_v6_LONG_eval \
+#     -c config/OV_COCO/OVDQUO_RN50.py \
+#     --options dataset_file=ovcoco text_len=77 \
+#     --eval \
+#     --resume logs/proposed_r50_v6_dualstream/checkpoint.pth
+
+
 #!/bin/bash
 
 export TORCH_ALLOW_CLASSES="argparse.Namespace"
 
-# Notice the --eval flag and the --resume pointing to your finished V6 weights
 torchrun --nproc_per_node=1 --master_port=29500 main.py \
-    --output_dir logs/proposed_r50_v6_LONG_eval \
+    --output_dir logs/proposed_r50_v7_fusion_LONG_eval \
     -c config/OV_COCO/OVDQUO_RN50.py \
     --options dataset_file=ovcoco text_len=77 \
     --eval \
-    --resume logs/proposed_r50_v6_dualstream/checkpoint.pth
+    --resume logs/proposed_r50_v7_fusion/checkpoint.pth
